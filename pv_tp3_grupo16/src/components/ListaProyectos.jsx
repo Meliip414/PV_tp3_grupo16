31f9e899ab29ce7d1fd6f0e763e9c4f2b697fffa
Meliip414 <mmalexandra2019@hotmail.com>
2026-05-12 20:02:59 -0300

    Agregado proyecto React, carpetas y archivos pedidos en el TP - Melina Leaño

---
+++ pv_tp3_grupo16/src/components/ListaProyectos.jsx
@@ -0,0 +1,12 @@
+import '../css/index.css';
+import proyectoService from '../services/proyectoService';
+// importar el estado prox
+
+const Proyectos = () => {
+
+    //variable que contiene la lista de los proyectos
+
+    //return(
+    //HTML formulario para ingresar y agregar un proyecto
+    //)
+}
