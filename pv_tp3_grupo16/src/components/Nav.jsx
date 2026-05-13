25f7f64c297052d69f758dc0f28b1e1987f5f805
Meliip414 <mmalexandra2019@hotmail.com>
2026-05-13 16:46:45 -0300

    Actualización de Nav.jsx - Melina Leaño

--- pv_tp3_grupo16/src/components/Nav.jsx
+++ pv_tp3_grupo16/src/components/Nav.jsx
@@ -1,13 +1,17 @@
 const Nav = () => {
-    return (
-        <nav>
-            <ul>
-                <a href="#">Inicio</a> -
-                <a href="#">Proyectos</a> -
-                <a href="#">Perfil</a>
-            </ul>
-        </nav>
-    );
+   
+   const menu = ["Inicio", "Proyectos", "Perfil"];
+
+   return (
+    <nav>
+        <ul>
+            {menu.map((men) => (
+                <a key={men} href="#">{men}</a>
+            ))}
+        </ul>
+    </nav>
+   )
+
 };
 
 export default Nav;
