61504a693edeac8011117b3466883fc233ad247b
BarbaraVillena <barbimorevi@gmail.com>
2026-05-12 21:18:42 -0300

    Creacion de arreglo de proyectos - Villena Luisana Bárbara

---
+++ pv_tp3_grupo16/src/services/proyectoService.js
@@ -0,0 +1,11 @@
+const gestionproyecto = (() => {
+    const proyecto=[
+        {id: 1, titulo:"Desarrollo de Sitio Web", categoria:"Desarrollo", estado:"En curso"},
+        {id: 2, titulo:"Creacion de papel reciclado", categoria:" Aprendizaje", estado:"Finalizado"},
+        {id: 3, titulo:"Control de Matriz LED 8x8 con Arduino", categoria:"Aprendizaje", estado:"Finalizado"},
+        {id: 4, titulo:"Combate por turnos", categoria:"Desarrollo", estado:"Finalizado"},
+        {id: 5, titulo:"Control manual LED RGB", categoria:"Aprendizaje", estado:"Finalizado"},
+        
+    ];
+}
+)
