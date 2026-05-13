8b8ce71e06547f9480c778746252a406f63c89e2
Meliip414 <mmalexandra2019@hotmail.com>
2026-05-13 16:49:43 -0300

    Modificaciones en Nav.jsx - Melina Leaño

--- pv_tp3_grupo16/src/components/ListaProyectos.jsx
+++ pv_tp3_grupo16/src/components/ListaProyectos.jsx
@@ -7,113 +7,25 @@ const ListaProyectos = () => {
     const [proyectos, setProyectos] = useState(
         proyectoService.obtenerProyectos()
     );
-    const [busqueda, setBusqueda] = useState("");
-
-    const [titulo, setTitulo] = useState("");
-
-    const [categoria, setCategoria] = useState("");
-
-    const [estado, setEstado] = useState("");
- 
-    const eliminar = (id) => {
-    proyectoService.eliminarProyecto(id);
-    setProyectos(proyectoService.obtenerProyectos());
-    }
-
-    const buscar = (texto) => {
-    setBusqueda(texto);
-    if(texto === ""){
-        setProyectos(proyectoService.obtenerProyectos());
-    }else{
-        setProyectos(proyectoService.buscarProyecto(texto));
-    }
-    }
-
-    const agregar = () => {
-    const nuevoProyecto = {
-        titulo,
-        categoria,
-        estado
-    };
-    proyectoService.agregarProyecto(nuevoProyecto);
-    setProyectos(proyectoService.obtenerProyectos());
-    setTitulo("");
-    setCategoria("");
-    setEstado("");
-    }
 
      return(
         <div>
 
-            <h2>LISTA DE PROYECTOS</h2>
+            <h2>Lista de proyectos</h2>
 
-           <div className="buscador">
-             <input
-             type="text"
-             placeholder="Buscar proyecto"
-             value={busqueda}
-             onChange={(e) => buscar(e.target.value)}
-            />
-            </div>
+            {proyectos.map((proyecto) => (
+                <div key={proyecto.id}>
 
-            <hr />
+                    <h3>{proyecto.titulo}</h3>
 
-            <div className="contenedor-principal">
+                    <p>Categoría: {proyecto.categoria}</p>
 
-            <div className="formulario">
-            <h3>AGREGAR PROYECTO</h3>
+                    <p>Estado: {proyecto.estado}</p>
 
-             <input
-             type="text"
-             placeholder="Título"
-             value={titulo}
-             onChange={(e) => setTitulo(e.target.value)}
-            />
+                </div>
+            ))}
 
-             <input
-             type="text"
-             placeholder="Categoría"
-             value={categoria}
-             onChange={(e) => setCategoria(e.target.value)}
-            />
-
-             <input
-             type="text"
-             placeholder="Estado"
-             value={estado}
-             onChange={(e) => setEstado(e.target.value)}
-            />
-
-            <button className="btn-agregar" onClick={agregar}>
-            AGREGAR PROYECTO
-            </button>
         </div>
-
-        <div className="lista-cards">
-
-        {proyectos.map((proyecto) => (
-            <div className="card" key={proyecto.id}>
-
-                <h3>{proyecto.titulo}</h3>
-
-                <p>Categoría: {proyecto.categoria}</p>
-
-                <p>Estado: {proyecto.estado}</p>
-
-                <button
-                    className="btn-eliminar"
-                    onClick={() => eliminar(proyecto.id)}
-                >
-                    Eliminar
-                </button>
-
-            </div>
-        ))}
-
-    </div>
-
-</div>
-</div>
     )
 }
 
