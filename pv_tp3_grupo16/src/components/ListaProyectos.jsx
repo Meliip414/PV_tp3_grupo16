414c3377b2b05d2fcbe2280b41529d7d5c7c1faf
apzzoe <apazazoe8@gmail.com>
2026-05-13 01:59:55 -0300

    Actualizado ListaProyectos.jsx -Zelaya Apaza Zoe

--- pv_tp3_grupo16/src/components/ListaProyectos.jsx
+++ pv_tp3_grupo16/src/components/ListaProyectos.jsx
@@ -7,11 +7,31 @@ const ListaProyectos = () => {
     const [proyectos, setProyectos] = useState(
         proyectoService.obtenerProyectos()
     );
+    const [busqueda, setBusqueda] = useState("");
+    const eliminar = (id) => {
+    proyectoService.eliminarProyecto(id);
+    setProyectos(proyectoService.obtenerProyectos());
+    }
+    const buscar = (texto) => {
+    setBusqueda(texto);
+    if(texto === ""){
+        setProyectos(proyectoService.obtenerProyectos());
+    }else{
+        setProyectos(proyectoService.buscarProyecto(texto));
+    }
+    }
 
      return(
         <div>
 
             <h2>Lista de proyectos</h2>
+
+            <input
+            type="text"
+            placeholder="Buscar proyecto"
+            value={busqueda}
+            onChange={(e) => buscar(e.target.value)}
+            />
 
             {proyectos.map((proyecto) => (
                 <div key={proyecto.id}>
@@ -21,6 +41,10 @@ const ListaProyectos = () => {
                     <p>Categoría: {proyecto.categoria}</p>
 
                     <p>Estado: {proyecto.estado}</p>
+
+                    <button onClick={() => eliminar(proyecto.id)}>
+                       Eliminar
+                    </button>
 
                 </div>
             ))}
