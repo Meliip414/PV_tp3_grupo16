d23918ec66f87951f01c66b9a72a90cf0bb48d5a
MariLujanButron17 <marianalujanbutron@gmail.com>
2026-05-13 00:22:41 -0300

    Agregado contenido en nav.jsx - Lujan Butron

---
+++ pv_tp3_grupo16/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <a href="#">Inicio</a> -
+                <a href="#">Proyectos</a> -
+                <a href="#">Perfil</a>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
