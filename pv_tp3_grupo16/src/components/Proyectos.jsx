6e1b100dc4e9b4854a827f634497d13d69c76b94
BarbaraVillena <barbimorevi@gmail.com>
2026-05-20 12:57:27 -0300

    Creacion de cerrarproyectos - Villena Luisana Bárbara

---
+++ pv_tp3_grupo16/src/components/Proyectos.jsx
@@ -0,0 +1,65 @@
+import '../css/index.css';
+import proyectoService from '../services/proyectoService';
+import { useState } from 'react';
+import FormProyecto from './FormProyecto';
+import DetalleProyecto from './DetalleProyecto';
+
+const Proyectos = () => {
+const [proyectos, setProyectos] = useState(
+    proyectoService.obtenerProyectosVisibles()
+);
+
+const [busqueda, setBusqueda] = useState("");
+const [titulo, setTitulo] = useState("");
+const [categoria, setCategoria] = useState("");
+const [estado, setEstado] = useState("");
+const [proyectoSeleccionado,setProyectoSeleccionado] = useState(null);
+
+
+const eliminar = (id) => {
+    proyectoService.eliminarProyecto(id);
+    setProyectos(proyectoService.obtenerProyectos());
+}
+
+const buscar = (texto) => {
+    setBusqueda(texto);
+    if (texto === "") {
+        setProyectos(proyectoService.obtenerProyectos());
+    } else {
+        setProyectos(proyectoService.buscarProyecto(texto));
+    }
+}
+
+const agregar = () => {
+    const nuevoProyecto = {
+        titulo,
+        categoria,
+        estado
+    };
+    proyectoService.agregarProyecto(nuevoProyecto);
+    setProyectos(proyectoService.obtenerProyectos());
+    setTitulo("");
+    setCategoria("");
+    setEstado("");
+}
+
+
+
+const cerrarProyecto = () => {
+    setProyectoSeleccionado(null);
+}
+
+
+return (
+    <div>
+        <FormProyecto 
+        
+        />
+        <DetalleProyecto 
+            proyecto={proyectoSeleccionado}
+            cerrarProyecto={cerrarProyecto} 
+        />
+    </div>
+)
+}
+export default Proyectos;
