f0f7c5ac4239fa0258cdcba6ebd50b237d03647f
Meliip414 <mmalexandra2019@hotmail.com>
2026-06-04 04:07:41 -0300

    Modificaciones a main.jsx - Melina Leaño

--- pv_tp3_grupo16/src/main.jsx
+++ pv_tp3_grupo16/src/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import './index.css'
-//import App from './App.jsx'
-import App2 from './App2.jsx'
+import { RouterProvider } from 'react-router-dom';
+import routes from './routes/routes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App2 />
+    <RouterProvider router={routes} />
   </StrictMode>,
 )
