63b56c57d817f7a149275cbdaabca37fe37c79eb
BarbaraVillena <barbimorevi@gmail.com>
2026-06-11 01:34:54 -0300

    buscar usuario en proyectoService.jsx- Villena Luisana Bárbara

--- pv_tp3_grupo16/src/services/proyectoService.js
+++ pv_tp3_grupo16/src/services/proyectoService.js
@@ -166,7 +166,22 @@ const gestionProyecto = (() => {
         {dni: '48144965', nombre: 'Manuelita', password:'viviaenpeguajo' , rol: 'alumno', institucion:'Escuela de Minas'}
     ];
 
-   
+    const login = (dni, password) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const encontrado = usuarios.find(
+                    u => u.dni === dni && u.password === password
+                );
+                if (encontrado) {
+                    resolve({ dni: encontrado.dni, nombre: encontrado.nombre , rol:encontrado.rol, institucion:encontrado.institucion});
+                } else {
+                    reject(new Error('Usuario o contraseña incorrectos'));
+                }
+            }, 800);
+        });
+    };
+
+
 
     const obtenerProyectosVisibles = () => {
         return proyectos.filter(p => p.visibilidad === true);
