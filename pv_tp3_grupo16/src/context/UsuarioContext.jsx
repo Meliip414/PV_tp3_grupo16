9e5be20d86159902f8a1fa2c40d2fc8ba07f42b4
MariLujanButron17 <marianalujanbutron@gmail.com>
2026-06-11 02:51:18 -0300

    Toque final a UsuarioContext - Lujan Butron

--- pv_tp3_grupo16/src/context/UsuarioContext.jsx
+++ pv_tp3_grupo16/src/context/UsuarioContext.jsx
@@ -1,20 +1,12 @@
 import { createContext, useState } from "react";
-
 export const UsuarioContext = createContext(null);
-
-const usuario = {
-  nombre: "Manuelita",
-  dni: "48753446",
-  rol: "Estudiante",
-  institucion: "Facultad de Ingeniería"
-};
 
 export const UsuarioProvider = ({ children }) => {
 
   const [usuarioActivo, setUsuarioActivo] = useState(null);
 
   const guardarSesion = () => {
-    setUsuarioActivo(usuario);
+    setUsuarioActivo(usuarios);
   };
 
   const cerrarSesion = () => {
